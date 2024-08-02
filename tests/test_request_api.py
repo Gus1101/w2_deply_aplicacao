471c9c827d479d9d1a2d85c940053abe9058a23b
Gus1101 <augustosantosb60@gmail.com>
2024-08-01 22:47:24 -0300

    add products endpoint and its tests

--- tests/test_request_api.py
+++ tests/test_request_api.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi.testclient import TestClient
-from main import app
+from main import *
 
 client = TestClient(app)
 
@@ -13,3 +13,13 @@ def test_request_api_status_code():
 def test_request_api_json():
     response = client.get("/")
     assert response.json() == {"Olá":"Mundo"}
+
+@pytest.mark.unit
+def test_request_lista_produtos_status_code():
+	response = client.get("/lista_de_produtos")
+	assert response.status_code == 200
+
+@pytest.mark.unit
+def test_request_lista_produtos_length():
+	response = client.get("/lista_de_produtos")
+	assert len(response.json()) == 3
